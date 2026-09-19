f3e61bb87033cbe0a782aeacabcdd4d0dc9b91c3
subhanmaulana1981 <subhanmaulana1981@gmail.com>
2026-09-19 19:03:10 +0700

    delete the https prefix in base url and change http method into https

--- lib/layanans/layanan_agenda.dart
+++ lib/layanans/layanan_agenda.dart
@@ -24,7 +24,7 @@ class LayananAgenda extends ChangeNotifier {
     notifyListeners();
 
     try {
-      Uri uriUrl = Uri.http(_stringUrl);
+      Uri uriUrl = Uri.https(_stringUrl);
       final response = await http.get(uriUrl, headers: {
         'Content-Type': 'application/json'
       });
